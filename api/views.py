e38bdf7fdddca6987b734ef536ce7d07adbed1c2
thibault <thibault.grandjean@icloud.com>
2020-07-15 08:57:47 +0200

    :sparkles: AuthToken

--- api/views.py
+++ api/views.py
@@ -2,12 +2,20 @@ import pickle
 import os
 import pandas as pd
 
-from rest_framework.decorators import api_view
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 
-from default_risk_API.settings import BASE_DIR
 from api.serializers import PredictionSerializer
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+        
 # Create your views here.
 
 class Predict(APIView):
@@ -18,14 +26,14 @@ class Predict(APIView):
     serializer_class = PredictionSerializer
 
     def post(self, request):
-        with open(os.path.join(BASE_DIR, 'models', 'model.pickle'), 'rb') as f:
+        with open(os.path.join(settings.BASE_DIR, 'models', 'model.pickle'), 'rb') as f:
             m = pickle.load(f)
         data = PredictionSerializer(data=request.data)
         data.is_valid()
         sk_id_curr = data.data['app_id']
         amt = data.data['amt']
         amt_annuity = data.data['amt_annuity']
-        data = pd.read_csv(os.path.join(BASE_DIR, 'models', 'test.csv'))
+        data = pd.read_csv(os.path.join(settings.BASE_DIR, 'models', 'test.csv'))
         data.set_index('SK_ID_CURR', inplace=True)
         res = {"score": (1 - m.predict(data.loc[sk_id_curr])[0]) * 100}
         return Response(res)
@@ -37,6 +45,6 @@ class Application(APIView):
     return data relative to an application.
     """
     def get(self, request, pk):
-        app_data = pd.read_csv(os.path.join(BASE_DIR, 'models', 'test.csv'))
+        app_data = pd.read_csv(os.path.join(settings.BASE_DIR, 'models', 'test.csv'))
         app_data.set_index('SK_ID_CURR', inplace=True)
         return Response(app_data.loc[pk].to_dict())
--- default_risk_API/settings.py
+++ default_risk_API/settings.py
@@ -28,7 +28,10 @@ else:
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
-ALLOWED_HOSTS = ['ec2-15-188-10-127.eu-west-3.compute.amazonaws.com']
+if DEBUG:
+    ALLOWED_HOSTS = ['127.0.0.1']
+else:
+    ALLOWED_HOSTS = ['ec2-15-188-10-127.eu-west-3.compute.amazonaws.com']
 
 
 # Application definition
@@ -41,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'api'
 ]
 
@@ -128,5 +132,9 @@ STATIC_ROOT = 'staticfiles'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
     ]
 }
