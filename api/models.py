05b03527196ebf0e6c5f715003a2f6731d4cd53c
thibault <thibault.grandjean@icloud.com>
2020-07-24 09:20:11 +0200

    occupation is now a choice field

--- api/models.py
+++ api/models.py
@@ -10,9 +10,30 @@ class Application(models.Model):
         (CASH_LOAN, 'Cash')
     ]
 
+    OCCUPATION_TYPE_CHOICES = [('Laborers', 'Laborers'),
+                               ('Core staff', 'Core staff'),
+                               ('Accountants', 'Accountants'),
+                               ('Managers', 'Managers'),
+                               ('unknown', 'unknown'),
+                               ('Drivers', 'Drivers'),
+                               ('Sales staff', 'Sales staff'),
+                               ('Cleaning staff', 'Cleaning staff'),
+                               ('Cooking staff', 'Cooking staff'),
+                               ('Private service staff', 'Private service staff'),
+                               ('Medicine staff', 'Medicine staff'),
+                               ('Security staff', 'Security staff'),
+                               ('High skill tech staff', 'High skill tech staff'),
+                               ('Waiters/barmen staff', 'Waiters/barmen staff'),
+                               ('Low-skill Laborers', 'Low-skill Laborers'),
+                               ('Realty agents', 'Realty agents'),
+                               ('Secretaries', 'Secretaries'),
+                               ('IT staff', 'IT staff'),
+                               ('HR staff', 'HR staff')]
+
     sk_id_curr = models.IntegerField(primary_key=True, unique=True)
     days_birth = models.FloatField(help_text='Age of the appliant')
-    occupation_type = models.CharField(max_length=60)
+    occupation_type = models.CharField(max_length=60,
+                                       choices=OCCUPATION_TYPE_CHOICES)
     amt_income_total = models.FloatField(help_text='Total income of the appliant')
     amt_credit = models.FloatField()
     name_contract_type = models.CharField(max_length=16,
